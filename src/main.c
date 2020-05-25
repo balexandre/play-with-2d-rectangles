d4caab07b31ac44bfc18bff64a44c9734dd92fcb
Bruno Alexandre <bruno.in.dk@gmail.com>
2020-05-25 10:43:52 +0200

    created 2 ways of running the application

--- src/main.c
+++ src/main.c
@@ -6,16 +6,126 @@
 #include "gravity.h"
 #include "printing.h"
 
-// clear && gcc -Wall -Werror -o rectangles src/*.c && ./rectangles
-int main(int argc, char *argv[])
+void com_menu_de_opcoes()
 {
-    int num_objectos = 0;
+    system("cls||clear"); // limpa o ecran
+    int menu = 1, x, y, w, h, p;
+    int posicaoObjSeleccionado = -1;
 
+    int num_objectos = 0;
+    RECTANGULO *area = cria_objecto(0, 0, 80, 25);
+    RECTANGULO *objectos = malloc(sizeof(*objectos));
+
+    while (menu != 0)
+    {
+        printf("--------------------------------------------\n");
+        printf("[PLANO] %dx%d\n", area->largura, area->altura);
+        for (int iObj = 0; iObj < num_objectos; iObj += 1)
+        {
+            RECTANGULO obj = objectos[iObj];
+            printf("[RECTANGULO %d] x(%d) y(%d), %dx%d\n",
+               iObj + 1, obj.x0, obj.y0, obj.largura, obj.altura);
+        }
+        printf("--------------------------------------------\n");
+
+        printf("digite '0' para SAIR\n");
+        printf("digite '1' para modificar PLANO inicial\n");
+        printf("digite '2' para CRIAR rectangulo\n");
+        printf("digite '3' para MOVER rectangulo para a ESQUERDA (<<)\n");
+        printf("digite '4' para MOVER rectangulo para a DIREITA (>>)\n");
+        printf("digite '5' para RODAR conteúdo do plano\n");
+        printf("digite '6' para MOSTRAR plano com rectangulos\n");
+        scanf("%d", &menu);
+        switch (menu)
+        {
+            case 1:
+                printf("\n[DEFINIR plano]");
+                printf("\n digite largura: ");
+                scanf("%d", &w);
+                printf(" digite altura: ");
+                scanf("%d", &h);
+                area = cria_objecto(0, 0, w, h);
+                system("cls||clear"); // limpa o ecran
+                break;
+            case 2:
+                printf("\n[CRIAR rectangulo]");
+                printf("\n digite a posicao inicial x: ");
+                scanf("%d", &x);
+                printf(" digite a posicao inicial y: ");
+                scanf("%d", &y);
+                printf(" digite largura: ");
+                scanf("%d", &w);
+                printf(" digite altura: ");
+                scanf("%d", &h);
+
+                RECTANGULO *objecto = cria_objecto(x, y, w, h);
+                num_objectos += 1; // aumenta o numero total de objectos
+                objectos = realloc(objectos, sizeof(*objectos) * num_objectos);
+                objectos[num_objectos-1] = *objecto;
+                activaGravidade(objectos, num_objectos);
+
+                system("cls||clear"); // limpa o ecran
+                break;
+            case 3:
+                printf("\n[MOVER rectangulo ESQUERDA (<<)]");
+                printf("\n digite a posicao x: ");
+                scanf("%d", &x);
+                printf(" digite a posicao y: ");
+                scanf("%d", &y);
+                printf(" digite as posicoes a mover: ");
+                scanf("%d", &p);
+
+                posicaoObjSeleccionado = seleccionaObjecto(x, y, objectos, num_objectos);
+                if (posicaoObjSeleccionado >= 0)
+                {
+                    RECTANGULO *a = moveObjectoParaEsquerda(objectos[posicaoObjSeleccionado], p);
+                    objectos[posicaoObjSeleccionado] = *a;
+                }
+                activaGravidade(objectos, num_objectos);
+
+                system("cls||clear"); // limpa o ecran
+                break;
+            case 4:
+                printf("\n[MOVER rectangulo DIREITA (>>)]");
+                printf("\n digite a posicao x: ");
+                scanf("%d", &x);
+                printf(" digite a posicao y: ");
+                scanf("%d", &y);
+                printf(" digite as posicoes a mover: ");
+                scanf("%d", &p);
+
+                posicaoObjSeleccionado = seleccionaObjecto(x, y, objectos, num_objectos);
+                if (posicaoObjSeleccionado >= 0)
+                {
+                    RECTANGULO *a = moveObjectoParaEsquerda(objectos[posicaoObjSeleccionado], p);
+                    objectos[posicaoObjSeleccionado] = *a;
+                }
+
+                activaGravidade(objectos, num_objectos);
+                system("cls||clear"); // limpa o ecran
+                break;
+            case 5:
+                imprimeTitulo("Plano rodado 90 graus\n\n");
+                rodaObjectos(objectos, num_objectos, area->largura);
+                activaGravidade(objectos, num_objectos);
+                break;
+            case 6:
+                imprimeTitulo("Plano com gravidade activada\n\n");
+                desenha(area, objectos, num_objectos);
+                break;
+        }
+    }
+}
+
+void com_instrucoes()
+{
+    system("cls||clear"); // limpa o ecran
+
+    int num_objectos = 0;
     // area do plano
     RECTANGULO *area = cria_objecto(0, 0, 30, 20);
-
     // rectangulos
-    RECTANGULO *objecto1 = cria_objecto(1, 1, 12, 5);
+    RECTANGULO *objecto1 = cria_objecto(1, 3, 12, 5);
     num_objectos += 1;
     RECTANGULO *objecto2 = cria_objecto(6, 5, 11, 3);
     num_objectos += 1;
@@ -75,12 +185,41 @@ int main(int argc, char *argv[])
     imprimeTitulo("Com rotacao a +90 graus e gravidade activada\n\n");
     desenha(area, objectos, num_objectos);
     // #endregion
+}
+
+// clear && gcc -Wall -Werror -o rectangles src/*.c && ./rectangles
+int main(int argc, char *argv[])
+{
+    int menuInicial = 1;
+
+    printf("--------------------------------------------\n");
+    printf("Opcao 1: Ver programa com menu\n");
+    printf(" > mostra um menu onde pode criar e mover objectos assim como definir o plano\n");
+    printf("Opcao 2: Ver programa com inscrucoes pre-definidas\n");
+    printf(" > cria um plano 30x20\n");
+    printf(" > cria um rectangulo 1,3+12,5\n");
+    printf(" > cria um rectangulo 6,5+11,3\n");
+    printf(" > cria um rectangulo 15,10+6,3\n");
+    printf(" > imprime posicoes originais\n");
+    printf(" > activa gravidade\n");
+    printf(" > imprime novas posicoes\n");
+    printf(" > roda o conteudo do plano 90 graus\n");
+    printf(" > imprime novas posicoes originais\n");
+    printf(" > activa gravidade\n");
+    printf(" > imprime novas posicoes\n");
+    printf(" > roda o conteudo do plano 90 graus\n");
+    printf(" > imprime novas posicoes originais\n");
+    printf(" > activa gravidade\n");
+    printf(" > imprime novas posicoes\n");
+    printf("Qualquer outro número para Sair\n");
+    scanf("%d", &menuInicial);
+    
+    if (menuInicial == 1)
+        com_menu_de_opcoes();
+    else if (menuInicial == 2)
+        com_instrucoes();
 
     exit(EXIT_SUCCESS);
 }
 
-/*
-** References
-** - adding color to text
-**   http://web.theurbanpenguin.com/adding-color-to-your-output-from-c/
-*/
+/* EOF */
